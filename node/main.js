30c64c663d5a6231567e3df9dc87880519b0a2d3
04Mansigarg <garg6446@gmail.com>
2022-04-14 14:20:38 +0530

    node

---
+++ node/main.js
@@ -0,0 +1,9 @@
+const addition = require("./add")
+const sub = require("./subtract")
+const multiply = require("./multiply")
+const divide = require("./divide")
+
+console.log(addition.add(6, 5))
+console.log(sub.subtract(6, 5))
+console.log(multiply.multiply(6, 5))
+console.log(divide.divide(9, 3))
